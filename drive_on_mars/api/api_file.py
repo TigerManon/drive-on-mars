13a582b60f1c587aba7417b06fa08cc0a01d9f1c
Kany Diakhate <kany.diakhate@gmail.com>
2023-12-11 13:02:32 +0100

    test api.get mvp

--- drive_on_mars/api/api_file.py
+++ drive_on_mars/api/api_file.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+import numpy as np
 
 api = FastAPI()
 
 # define a root `/` endpoint
 @api.get("/")
 def index():
-    return {"ok": "API connected"}
+    return {"ok": "API connected check"}
 
 
 @api.get("/predict")
@@ -18,3 +19,16 @@ def predict(feature1, feature2):
     # In a real life setting, you would return the predictions.
 
     return {'prediction': int(feature1)*int(feature2)}
+
+@api.get("/mvp")
+def mvp(nb1: int, nb2: int, nb3: int, nb4: int):
+
+    arr = np.array([[nb1, nb2], [nb3, nb4]])
+
+    # fct inversement tab (à vérifier)
+    flip_flop = np.flip(arr)
+
+    # Convert° en liste de listes
+    flipped_list = flip_flop.tolist()
+
+    return {"flipped_array": flipped_list}
